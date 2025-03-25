eac906b804fe114553de21878b905a36012083ad
Elizabeth Pogreba <500192896@fvtc.edu>
2025-03-25 11:47:02 -0500

    Commit 2

--- CPP-TicTacToe/TicTacToe.h
+++ CPP-TicTacToe/TicTacToe.h
@@ -3,7 +3,17 @@
 
 class Something {
 
-public: 
+public:
+	char m_board[9] = {};
+
+	char m_Row1[6] = { ' ', '|', ' ', '|', ' ', '\0' };
+	char m_Divide1[6] = { '-', '-', '-', '-', '-', '\0' };
+	char m_Row2[6] = { ' ', '|', ' ', '|', ' ', '\0' };
+	char m_Divide2[6] = { '-', '-', '-', '-', '-', '\0' };
+	char m_Row3[6] = { ' ', '|', ' ', '|', ' ', '\0' };
 
 
-}
+	virtual void Display() const { std::cout << m_Row1 << "\n" << m_Divide1 << "\n" << m_Row2 << "\n" << m_Divide2 << "\n" << m_Row3; }
+
+
+};
